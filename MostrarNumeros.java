5f32413cdfa9c9c2efc502e183f60723d507662b
JAaron-Urgel <juavacurg@alu.edu.gva.es>
2025-11-19 12:00:45 +0100

    Ejercicio8

--- MostrarNumeros.java
+++ MostrarNumeros.java
@@ -13,4 +13,34 @@ public class MostrarNumeros
         System.out.println("Número2: " + numero2);
         sc.close();
     }
+
+    public static double restar(double a, double b)
+    {
+        return a-b;
+    }
+
+    public static double dividir(double a, double b)
+    {
+        if(b!=0)
+        {
+            return a/b;
+        }
+        else
+        {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+    }
+
+    public static double raizCuadrada(double a)
+    {
+        if (a>=0)
+        {
+            return Math.sqrt(a);
+        }
+        else
+        {
+            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negrativo");
+        }
+
+    }
 }
